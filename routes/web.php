e777d2f53ea1b7c9a64178addba931e3120eb58f
Pavel Bogachev <boga4ev13@mail.ru>
2023-02-14 00:35:18 +0300

    243165 / Детальная страница новости - создана страница для отдельной новости

--- routes/web.php
+++ routes/web.php
@@ -10,3 +10,4 @@ Route::get('finance-department',   ['uses' => 'PagesController@financeDepartment
 Route::get('for-clients',          ['uses' => 'PagesController@forClients',           'as' => 'for-clients']);
 Route::get('salons',               ['uses' => 'PagesController@salons',               'as' => 'salons']);
 Route::get('articles',             ['uses' => 'PagesController@articles',             'as' => 'articles']);
+Route::get('articles/{article}',   ['uses' => 'PagesController@show',                 'as' => 'article-show']);
