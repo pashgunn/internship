b03a8a6ba5c423c24105876aece191b509e81a1c
Pavel Bogachev <boga4ev13@mail.ru>
2023-02-16 17:35:54 +0300

    243166 / Исправления - исправлена типовая ошибка неправильной работы поля с сессией или реквестом

--- resources/views/components/input/text.blade.php
+++ resources/views/components/input/text.blade.php
@@ -1,2 +1,4 @@
 @props(['error'])
-<input type="text" {{ $attributes->class(['mt-1 block w-full rounded-md', ($error ? 'border-red-600' : 'border-gray-300')]) }} value="{{ old($attributes['name']) }}" class="shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" placeholder="">
+<input type="text"
+       {{ $attributes->class(['mt-1 block w-full rounded-md', ($error ? 'border-red-600' : 'border-gray-300')]) }} class="shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+       placeholder="">
--- resources/views/components/input/textarea.blade.php
+++ resources/views/components/input/textarea.blade.php
@@ -1,1 +1,3 @@
-<textarea {{ $attributes }} {{ $attributes->class(['mt-1 block w-full rounded-md', ($error ? 'border-red-600' : 'border-gray-300')]) }} class="shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" rows="3">{{ old($name) }}</textarea>
+<textarea
+    {{ $attributes }} {{ $attributes->class(['mt-1 block w-full rounded-md', ($error ? 'border-red-600' : 'border-gray-300')]) }} class="shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+    rows="3">{{ $slot }}</textarea>
